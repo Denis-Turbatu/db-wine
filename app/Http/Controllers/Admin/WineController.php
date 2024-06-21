c4498f053cbefdac3d72f7c0e4bd12ea7351c99f
Denis-Turbatu <turbatudenis81@gmail.com>
2024-06-21 16:51:23 +0200

    feat: indx + indexController

--- app/Http/Controllers/Admin/WineController.php
+++ app/Http/Controllers/Admin/WineController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Wine;
 class WineController extends Controller
 {
     /**
@@ -12,7 +12,8 @@ class WineController extends Controller
      */
     public function index()
     {
-        return view('admin.wines.index');
+        $wines = Wine::all();
+        return view('admin.wines.index', compact('wines'));
     }
 
     /**
--- resources/views/admin/wines/index.blade.php
+++ resources/views/admin/wines/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>index</h1>
+
+
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center my-5">
+            <h1>Lista Wines</h1>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        
+                        <th scope="col">Winery</th>
+                        <th scope="col">Wine</th>
+                        <th scope="col">Rating</th>
+                        <th scope="col">Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($wines as $wine)
+                        <tr>
+                            <td>{{$wine->winery}}</td>
+                            <td>{{$wine->wine}}</td>
+                            <td>{{$wine->rating}}</td>
+                            <td>{{$wine->location}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
